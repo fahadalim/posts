57c63a767f46b48c2599dc0bc8f7a39768b826b0
fahadalim <mohammadfahadalim@gmail.com>
2022-08-09 23:26:37 +0530

    ghyj

---
+++ api.js
@@ -0,0 +1,37 @@
+const client = require("./connection.js");
+const express = require("express");
+require("dotenv").config()
+var cors = require("cors");
+const app = express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(cors());
+app.listen(process.env.PORT||5500, () => {
+  console.log("Sever is now listening at port 3300");
+});
+
+client.connect();
+
+app.get("/users", (req, res) => {
+  client.query(`Select * from users ORDER BY id DESC`, (err, result) => {
+    if (!err) {
+      res.send(result.rows);
+    }
+  });
+  client.end;
+});
+
+app.post("/users", (req, res) => {
+  const user = req.body;
+  let insertQuery = `insert into users(fro, too, body, time) 
+                     values('${user.fro}', '${user.too}', '${user.body}', '${user.time}')`;
+
+  client.query(insertQuery, (err, result) => {
+    if (!err) {
+      res.send("Insertion was successful");
+    } else {
+      console.log(err.message);
+    }
+  });
+  client.end;
+});
---
+++ connection.js
@@ -0,0 +1,13 @@
+const {Client} = require('pg')
+require("dotenv").config();
+
+const client = new Client({
+    host: "localhost",
+    user: "postgres",
+    connectionString: process.env.DATABASE_URL,
+    port: process.env.PORT||5432,
+    password: "Fahad@7277",
+    database: "postgres"
+})
+
+module.exports = client
