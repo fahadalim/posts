2e661a29a44bbac699fcb9e98896f3380b4ee4bc
fahadalim <mohammadfahadalim@gmail.com>
2022-08-10 09:56:12 +0530

    commit

--- api.js
+++ api.js
@@ -1,12 +1,12 @@
 const client = require("./connection.js");
 const express = require("express");
-require("dotenv").config()
+require("dotenv").config();
 var cors = require("cors");
 const app = express();
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
 app.use(cors());
-app.listen(process.env.PORT||5500, () => {
+app.listen(process.env.PORT || 5500, () => {
   console.log("Sever is now listening at port 3300");
 });
 
@@ -23,8 +23,8 @@ app.get("/users", (req, res) => {
 
 app.post("/users", (req, res) => {
   const user = req.body;
-  let insertQuery = `insert into users(fro, too, body, time) 
-                     values('${user.fro}', '${user.too}', '${user.body}', '${user.time}')`;
+  var insertQuery = `insert into users(fro, too, body, time)
+  values('${user.fro}', '${user.too}', '${user.body}', '${user.time}')`;
 
   client.query(insertQuery, (err, result) => {
     if (!err) {
@@ -35,3 +35,41 @@ app.post("/users", (req, res) => {
   });
   client.end;
 });
+
+// const client = require("./connection.js");
+// const express = require("express");
+// require("dotenv").config()
+// var cors = require("cors");
+// const app = express();
+// const bodyParser = require("body-parser");
+// app.use(bodyParser.json());
+// app.use(cors());
+// app.listen(process.env.PORT||5500, () => {
+//   console.log("Sever is now listening at port 3300");
+// });
+
+// client.connect();
+
+// app.get("/users", (req, res) => {
+//   client.query(`Select * from users ORDER BY id DESC`, (err, result) => {
+//     if (!err) {
+//       res.send(result.rows);
+//     }
+//   });
+//   client.end;
+// });
+
+// app.post("/users", (req, res) => {
+//   const user = req.body;
+//   let insertQuery = `insert into users(fro, too, body, time)
+//                      values('${user.fro}', '${user.too}', '${user.body}', '${user.time}')`;
+
+//   client.query(insertQuery, (err, result) => {
+//     if (!err) {
+//       res.send("Insertion was successful");
+//     } else {
+//       console.log(err.message);
+//     }
+//   });
+//   client.end;
+// });
--- connection.js
+++ connection.js
@@ -1,13 +1,13 @@
-const {Client} = require('pg')
+const { Client } = require("pg");
 require("dotenv").config();
 
 const client = new Client({
-    host: "localhost",
-    user: "postgres",
-    connectionString: process.env.DATABASE_URL,
-    port: process.env.PORT||5432,
-    password: "Fahad@7277",
-    database: "postgres"
-})
+  host: "localhost",
+  user: "postgres",
+  connectionString: process.env.DATABASE_URL,
+  port: process.env.PORT || 5432,
+  password: "Fahad@7277",
+  database: "postgres",
+});
 
-module.exports = client
+module.exports = client;
